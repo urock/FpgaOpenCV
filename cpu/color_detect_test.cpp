9492f4f1c8c1c1220cd2d14f269f37af32c06cff
Ruslan Bainazarov <hocop@yandex.ru>
2016-01-27 00:26:28 +0300

    Пример поиска одного объекта одного цвета в видео-файле

--- cpu/color_detect_test.cpp
+++ cpu/color_detect_test.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
+#include<fstream>
+#include<string>
+
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <opencv2/core/core.hpp>
-#include <opencv2/videoio/videoio.hpp>
 
 using namespace cv;
 using namespace std;
 
-// компиляция:
-// g++ color_test.cpp -lopencv_highgui -lopencv_imgproc -lopencv_videoio -lopencv_core
+// g++ color_test.cpp -lopencv_highgui -lopencv_imgproc -lopencv_core
 
 int main()
 {
 	VideoCapture cap;
-	cap.open("example.avi");
+	cap.open("example.AVI");
 	//cap.open(0);
 	if ( !cap.isOpened() )  // if not success, exit program
     {
@@ -21,17 +22,37 @@ int main()
          return -1;
     }
     
-	int iLowH = 4;
+	int iLowH = 2;
 	int iHighH = 27;
 	
-	int iLowS = 113; 
-	int iHighS = 195;
+	int iLowS = 200; 
+	int iHighS = 255;
 	
-	int iLowV = 83;
-	int iHighV = 220;
+	int iLowV = 70;
+	int iHighV = 255;
+	
+	// read settings from file
+	ifstream settings("settings");
+	if( settings.is_open() ){
+		string line;
+		stringstream ss(line);
+		
+		getline(settings, line);
+		ss >> iLowH;
+		getline(settings, line);
+		ss >> iHighH;
+		getline(settings, line);
+		ss >> iLowS;
+		getline(settings, line);
+		ss >> iHighS;
+		getline(settings, line);
+		ss >> iLowV;
+		getline(settings, line);
+		ss >> iHighV;
+	}
 	
 	Mat imgIn;
-	Mat imgOut;
+	Mat imgOut = imgIn;
 	
 	int iLastX = -1;
 	int iLastY = -1;
@@ -41,16 +62,24 @@ int main()
                   (int) cap.get(CV_CAP_PROP_FRAME_HEIGHT));
     
 	VideoWriter wr;
-	wr.open("output/ball.avi", -1, 30, S, true);
+	wr.open("output.AVI", cap.get(CV_CAP_PROP_FOURCC), cap.get(CV_CAP_PROP_FPS), S, true);
 	
 	if (!wr.isOpened()) {
 		cout  << "Could not open the output video for write" << endl;
 		return -1;
 	}
 	
+	ofstream cordsFile;
+	cordsFile.open("cords_output.txt");
+	
 	while(cap.read(imgIn)){
+		imgOut = imgIn;
+		
+		Mat imgHSV;
+		cvtColor(imgIn, imgHSV, COLOR_BGR2HSV);
+		
 		Mat imgThresholded;
-		inRange(imgIn, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded);
+		inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded);
 		
 		//morphological opening (removes small objects from the foreground)
 		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
@@ -67,23 +96,25 @@ int main()
 		double dM10 = oMoments.m10;
 		double dArea = oMoments.m00;
 		
-		if (dArea > 10000) {
+		if (dArea > 1000) {
 			//calculate the position of the ball
 			int posX = dM10 / dArea;
 			int posY = dM01 / dArea;        
 			if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0)
 			{
-			//Draw a red line from the previous point to the current point
-			line(imgOut, Point(posX, posY), Point(iLastX, iLastY), Scalar(0,0,255), 2);
+				//Draw a red line from the previous point to the current point
+				//line(imgOut, Point(posX, posY), Point(iLastX, iLastY), Scalar(0,0,255), 2);
+				line(imgOut, Point(posX, posY), Point(0, 0), Scalar(0,0,255), 2);
 			}
 			iLastX = posX;
 			iLastY = posY;
+			//Write coordinates to file
+			cordsFile << posX << ' ' << posY << '\n';
 		}
 		
 		wr.write(imgOut);
-		
-		cout << "i";
 	}
 	
+	cordsFile.close();
 	return 0;
 }
