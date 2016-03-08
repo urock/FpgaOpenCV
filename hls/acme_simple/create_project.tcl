84bbdd08d3d44fc2ccc8653789c51b1b996c78db
urock <yarumyantsev@gmail.com>
2016-03-08 14:12:19 +0300

    added first hls cores

---
+++ hls/acme_simple/create_project.tcl
@@ -0,0 +1,23 @@
+
+# Create the design project
+open_project acme_simple_prj -reset
+
+# Define the top level function for hardware synthesis
+set_top image_filter
+
+# Select the files for hardware synthesis
+add_files ./src/top.cpp
+
+
+# Set the name of the solution for this design
+open_solution "solution1"
+
+# Select the FPGA 
+set_part "xc7z020clg400-1"
+create_clock -period "150MHz"
+
+csynth_design
+#cosim_design
+export_design -format ip_catalog -vendor "urock" -library "fopencv" -version "0.1" -display_name "acme_simple"
+
+
