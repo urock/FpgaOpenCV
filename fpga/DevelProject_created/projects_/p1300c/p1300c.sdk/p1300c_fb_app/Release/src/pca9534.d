aef448a6b176c6b93a664099e87c467cadfebd46
tasha <tasha>
2016-03-14 01:27:51 +0300

    added accembled project

---
+++ fpga/DevelProject_created/projects_/p1300c/p1300c.sdk/p1300c_fb_app/Release/src/pca9534.d
@@ -0,0 +1,44 @@
+src/pca9534.o src/pca9534.d: ../src/pca9534.c ../src/pca9534.h \
+ ../../p1300c_fb_bsp/ps7_cortexa9_0/include/xiicps.h \
+ ../../p1300c_fb_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../p1300c_fb_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../p1300c_fb_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../p1300c_fb_bsp/ps7_cortexa9_0/include/xiicps_hw.h \
+ ../../p1300c_fb_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../p1300c_fb_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../p1300c_fb_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../p1300c_fb_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../p1300c_fb_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../p1300c_fb_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../p1300c_fb_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../src/xiicps_ext.h ../../p1300c_fb_bsp/ps7_cortexa9_0/include/xstatus.h
+
+../src/pca9534.h:
+
+../../p1300c_fb_bsp/ps7_cortexa9_0/include/xiicps.h:
+
+../../p1300c_fb_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../p1300c_fb_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../p1300c_fb_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../p1300c_fb_bsp/ps7_cortexa9_0/include/xiicps_hw.h:
+
+../../p1300c_fb_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../p1300c_fb_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../p1300c_fb_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../p1300c_fb_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../p1300c_fb_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../p1300c_fb_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../p1300c_fb_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../src/xiicps_ext.h:
+
+../../p1300c_fb_bsp/ps7_cortexa9_0/include/xstatus.h:
---
+++ fpga/DevelProject_created/projects_/p1300c/p1300c.sdk/zynq_fsbl_app/Debug/src/ps7_init.d
@@ -0,0 +1,29 @@
+src/ps7_init.o src/ps7_init.d: \
+ E:/users/tasha/git_dirs/refactor/FpgaOpenCV/fpga/DevelProject/projects_/p1300c/p1300c.sdk/p1300c_fb_hw/ps7_init.c \
+ E:/users/tasha/git_dirs/refactor/FpgaOpenCV/fpga/DevelProject/projects_/p1300c/p1300c.sdk/p1300c_fb_hw/ps7_init.h \
+ ../../zynq_fsbl_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../zynq_fsbl_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../zynq_fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../zynq_fsbl_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../zynq_fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../zynq_fsbl_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../zynq_fsbl_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../zynq_fsbl_bsp/ps7_cortexa9_0/include/xparameters_ps.h
+
+E:/users/tasha/git_dirs/refactor/FpgaOpenCV/fpga/DevelProject/projects_/p1300c/p1300c.sdk/p1300c_fb_hw/ps7_init.h:
+
+../../zynq_fsbl_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../zynq_fsbl_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../zynq_fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../zynq_fsbl_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../zynq_fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../zynq_fsbl_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../zynq_fsbl_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../zynq_fsbl_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
