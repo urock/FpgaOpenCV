79ce80f40ee83ca378eaa73ec53f22656a6fe7d2
Ruslan Bainazarov <hocop@yandex.ru>
2016-04-24 12:30:13 +0300

    добавил обучение

--- cpu/neuro/main.cpp
+++ cpu/neuro/main.cpp
@@ -3,27 +3,58 @@
 
 #include"neuro.hpp"
 #include"descriptor.hpp"
+#include"teacher.hpp"
 
 using namespace std;
 using namespace cv;
 
 int main() {
-	int lsz[] = {3, 2048, 2};
+	int lsz[] = {3, 2, 1};
 	int lN = sizeof(lsz) / sizeof(int);
 	
 	Neuro neuro( lN, lsz );
 	
 	Descriptor des( neuro );
-	//des.save( "data/neuro1" );
+	des.save( "data/before" );
 	cout << "data saved\n";
 	
-	//imwrite( "data/tst.png", des.getImage() );
-	cout << "pic saved\n";
+	flt input[] = {.1, .1, .1};
+	flt output[1];
 	
-	flt input[] = {1.0, 1.0, 1.0};
-	flt output[2];
 	neuro.predict(input, output);
-	cout << output[0] << '\t' << output[1] << '\n';
+	//cout << output[0] << '\t' << output[1] << '\n';
+	
+	Teacher teacher(neuro);
+	
+	vec inputV, theory;
+	for( int i = 0; i < lsz[0]; ++i )
+		inputV.push_back( input[i] );
+	
+	theory.push_back(.1);
+	
+	for( int i = 0; i < 1000010; ++i ) {
+		int sum = 0;
+		for( int k = 0; k < lsz[0]; ++k ) {
+			int s = rand() % 2;
+			sum += s;
+			inputV[k] = s;
+		}
+		
+		if( sum == 1 )
+			theory[0] = .5;
+		else
+			theory[0] = .0;
+		
+		teacher.teach(inputV, theory);
+		neuro.predict(&inputV[0], output);
+		if(i > 1000000)
+			cout << i << '\t' << sum << '\t' << output[0] << '\n';
+	}
+	
+	des.save( "data/after" );
+	cout << "data saved\n";
+	imwrite( "data/tst.png", des.getImage() );
+	cout << "pic saved\n";
 	
 	return 0;
 }
--- cpu/neuro/teacher.hpp
+++ cpu/neuro/teacher.hpp
@@ -1,20 +1,29 @@
 #ifndef TEACHER
 #define TEACHER
 
+#include<vector>
+#include<iterator>
+#include<cmath>
 
+#include"neuro.hpp"
 
+using namespace std;
 
+typedef vector<flt> vec;
 
-
-
-
-
-
-
-
-
-
-
+struct Teacher
+{
+	Teacher( Neuro &n ) : neuro(n) {}
+	
+	Neuro &neuro;
+	flt speed = 0.2;
+	
+//	flt error( const vec &real, const vec &theory );
+	vec errorDeriv( const vec &real, const vec &theory );
+	
+	void teachLayer( const vec &inputVal, int outputSz, const vec &deriv, flt **weight, vec &nextDeriv );
+	void teach( const vec &input, const vec &theory );
+};
 
 #endif //TEACHER
 
