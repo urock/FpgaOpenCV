e9b9d7caad933e74a4e794bf878a65bb3d9540a3
urock <yarumyanstev@gmail.com>
2016-04-27 12:47:23 +0300

    wip on hls float model

--- hls/neuro/neuro_fpga.cpp
+++ hls/neuro/neuro_fpga.cpp
@@ -7,12 +7,91 @@
 #define P      16       // параллельных вычислителей PE (processing elements)
 
 
+// Какой у меня интерфейс к памяти на microZed
+
+// 32 bits * 1 GT/s = 4 GB/s
+
+// 320 bits * 100 MT/s
+
+// 256 bits * 100 MHz
+
+// Если использовать тип данных для весов - 16 бит. 256/16 = 16 параллельных операций
+
+
+
+
+#define layersN 3
+
+void predict(float *image_in, float *weights_in, float *result) {
+
+   float local_buf[N];
+
+   // загружаем картинку 32*32 из DDR в локальный буфер BRAM
+   memcpy(local_buf, image_in, sizeof(float)*N);
+
+   for (int l = 0; l < layersN; l++)
+      compLayer(local_buf, l, weights_in, local_buf);
+
+   // тут что-то нужно сделать с результатом, выделить из него признак, нашли или нет объект.
+
+
+
+}
+
+
+void compLayer(float *data_in, int layer_num, float *weights_in, float *data_out) {
+
+   float       w_in[N/P][P];
+   float       linputs[N/P][P];
+
+   // load weights and make calc
+   for (int i = 0; i < N; ++i) {
+
+      float data_in_val = data_in[i];
+
+      // для каждого входного данного надо подгрузить из памяти N весов
+      memcpy(w_in, weights_in + layer_num*sizeof(float)*N*N + i*sizeof(float)*N, sizeof(float)*N);
+
+      // пусть у нас будет P = 16 параллельных вычислителей, тогда это два следующих вложенных цикла
+      for (int j = 0; j < N/P; ++j) {
+
+         // этот цикл будет развернут и выполнен параллельно
+         // средствами HLS будет обеспечен одновременный доступ ко всем элементам второго измерения массивов w_in и linputs
+         // кстати на CPU, где поддерживаются векторные операции тут тоже можно добиться параллельности
+         for (int k = 0; k < P; ++k) {
+
+            float w = w_in[j][k];
+
+            if (i == 0)
+               linputs[j][k] = w*data_in_val;
+            else
+               linputs[j][k] += w*data_in_val;
+
+         } // k
+      } // j
+   } // i
+
+   int l = 0;
+   // calc outputs of neurons
+   for (int j = 0; j < N/P; ++j) {
+      // этот цикл тоже может быть распараллелен
+      for (int k = 0; k < P; ++k) {
+         data_out[l++] = activation(linputs[j][k]);
+      }
+   }
+
+}
+
+
+
+
+
 typedef ap_uint<256>    wide_t;
 typedef int             input_t;
 
 
 // пока только для одного слоя
-void dummy_func(ap_int<256> *ddr_base ) {
+void dummy_func_hls(ap_int<256> *ddr_base ) {
 
 
    short img_in[N];  // 2 bytes per pixel
@@ -60,13 +139,3 @@ void dummy_func(ap_int<256> *ddr_base ) {
 
 
 }
-
-// Какой у меня интерфейс к памяти на microZed
-
-// 32 bits * 1 GT/s = 4 GB/s
-
-// 320 bits * 100 MT/s
-
-// 256 bits * 100 MHz
-
-// тип данных для весов - 16 бит. 256/16 = 16 параллельных операций
