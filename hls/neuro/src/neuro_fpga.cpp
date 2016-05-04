b91db71a026a28864eeecad269b4ee0532d5c4e5
urock <yarumyanstev@gmail.com>
2016-05-04 09:58:37 +0300

    proof of concept neuro hls synth ok with float and 8 parallel mults. Performanse 16 ms per 32*32 window. TODO: implement golden model in VHLS

--- hls/neuro/src/neuro_fpga.cpp
+++ hls/neuro/src/neuro_fpga.cpp
@@ -1,12 +1,8 @@
 
-#include <string.h>
-#include <stdio.h>
 #include "neuro.h"
 
 
-
-
-void neuro_top(volatile float_8_t *ddr_base ) {
+void neuro_top(volatile ddr_bus *ddr_base, float buf_out[N] ) {
 
 #pragma HLS INTERFACE m_axi port=ddr_base offset=direct
 #pragma HLS DATA_PACK variable=ddr_base struct_level
@@ -14,26 +10,42 @@ void neuro_top(volatile float_8_t *ddr_base ) {
 
    float local_buf[N];
 
-   float_8_t data_in;
-#pragma HLS DATA_PACK variable=data_in struct_level
+   ddr_bus 	input_buffer[D];
 
-   loop_in_1:for (int i = 0; i < N/8; i++) {
-      data_in = *(ddr_base + i);
-      loop_in_2: for (int j = 0; j < 8; j++)
-         local_buf[i*8 + j] = data_in.flt[i];
+   int_float ift;
+
+
+   for (int i = 0; i < L; i++) {
+
+		memcpy(input_buffer, (ddr_bus*)(ddr_base + i*D), sizeof(input_buffer));
+
+		   for (int k=0; k<D; k++) {
+			   for (int l=0; l<P; l++) {
+				   unsigned int temp32;
+				   temp32 = input_buffer[k].range(31+32*l,32*l);
+				   ift.A = temp32;
+				   local_buf[i*32 +k*8 +l] = ift.B;
+			   }
+		   }
+
+
+
    }
+
 
 
     loop_comp: for (int l = 0; l < layersN; l++)
       compLayer(local_buf, l, ddr_base, local_buf);
 
 
-   loop_out_1:for (int i = 0; i < N/8; i++) {
+   loop_out_1:for (int i = 0; i < N; i++) {
 
-      loop_out_2: for (int j = 0; j < 8; j++)
-         data_in.flt[i] = local_buf[i*8 + j];
+	   buf_out[i] = local_buf[i];
 
-       *(ddr_base + i) = data_in;
+//      loop_out_2: for (int j = 0; j < 8; j++)
+//    	  temp.B.d[i] = local_buf[i*8 + j];
+//
+//       *(ddr_base + i) = temp.A;
    }
 
 
@@ -42,43 +54,48 @@ void neuro_top(volatile float_8_t *ddr_base ) {
 }
 
 
-void compLayer(float *data_in, int layer_num, volatile float_8_t *ddr_base, float *data_out) {
+void compLayer(float *data_in, int layer_num, volatile ddr_bus *ddr_base, float *data_out) {
 
 
 
-   float     w_in[N/P][P];
-#pragma ARRAY_PARTITION linputs -dim 2 complete
+	   ddr_bus 	w_input_buffer[D];
+#pragma HLS ARRAY_PARTITION variable=w_input_buffer complete dim=1
 
-   float     linputs[N/P][P];
-#pragma ARRAY_PARTITION linputs -dim 2 complete
+	   int_float ift;
+
+
+   float     linputs[L*D][P];
+#pragma HLS ARRAY_PARTITION variable=linputs complete dim=2
+//#pragma ARRAY_PARTITION linputs -dim 2 complete
 
    // load weights and make calc
-   c_loop_i: for (int i = 0; i < N; ++i) {
+   l1: for (int i = 0; i < N; ++i) {
 
-#pragma HLS_DATAFLOW
+	   float data_in_val = data_in[i];
 
-      float data_in_val = data_in[i];
+	   l2: for (int j = 0; j < L; ++j) {
+#pragma HLS DATAFLOW
+ // подгружаем веса блоками по D*P, всего таких блоков будет L
 
-   // для каждого входного данного надо подгрузить из памяти N весов
-      memcpy(w_in, (float_8_t*)(ddr_base + layer_num*N*N/P + i*N/P), sizeof(float_8_t)*N/P);
-   // пусть у нас будет P параллельных вычислителей, тогда это два следующих вложенных цикла
-      c_loop_j: for (int j = 0; j < N/P; j++) {
-#pragma HLS_PIPELINE
+		   memcpy(w_input_buffer, (ddr_bus*)(ddr_base + 0x1000000 + layer_num*N*N/P + i*N/P + j*D), sizeof(w_input_buffer));
 
-         c_loop_k: for (int k = 0; k < P; ++k) {
+		   l3: for (int k=0; k<D; k++) {
+#pragma HLS PIPELINE
 
-            float w = w_in[j][k];
+			   l4: for (int l=0; l<P; l++) {
+				   unsigned int temp32;
+				   temp32 = w_input_buffer[k].range(31+32*l,32*l);
+				   ift.A = temp32;
 
-            //ap_uint<16> w = wide_weight.Range((k+1)*P - 1,k*P);
+		            if (i == 0)
+		               linputs[j*4 + k][l] = ift.B*data_in_val;
+		            else
+		               linputs[j*4 + k][l] += ift.B*data_in_val;
 
-            if (i == 0)
-               linputs[j][k] = w*data_in_val;
-            else
-               linputs[j][k] += w*data_in_val;
-
-         } // k
-      } // j
-   } // i
+			   } //l4
+		   } // l3
+	   } // l2
+   } // l1
 
 
    int l = 0;
@@ -97,23 +114,3 @@ float activation(float x) {
    return x;
 }
 
-
-// typedef struct {
-//    float d0;
-//    float d1;
-// } two_floats;
-
-
-// union long_float{
-//    uint64_t A;
-//    two_floats B;
-// };
-
-
-//    volatile uint64_t  *stream1;
-
-//    long_float temp;
-//    two_floats rand_buf2d;
-
-//       temp.A      = *stream1;
-//       rand_buf2d  = temp.B;
